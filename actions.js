8146141fff2f7a1776c67af1d6ac47fcd00d8f8d
Malgorzata Lesniak <M999@interia.eu>
2020-02-17 02:14:52 +0100

    Use parameter text

--- actions.js
+++ actions.js
@@ -24,7 +24,7 @@ export function editComment(commentId,text) {
     return {
         type: EDIT_COMMENT,
         id: commentId,
-        text: 'wyedytowany tekst komentarza'
+        text: text
     }
 }
 
